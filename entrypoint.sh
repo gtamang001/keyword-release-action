8f7a2472afc34ff1dc6fb764762376aaef47d4e3
ganga.tamang <ganga.bomzan@gmail.com>
2022-05-30 13:02:58 -0400

    test with both files added

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ then
 elif [ -f ./sample_push_event.json ];
 echo "Executing elif "
 then
+    echo "Executing setting up EVENT_PATH"
     EVENT_PATH='./sample_push_event.json'
     LOCAL_TEST=true
 else
