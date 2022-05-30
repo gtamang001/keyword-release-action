5f72b86592505b742403b280115ce037c620da31
ganga.tamang <ganga.bomzan@gmail.com>
2022-05-30 15:53:02 -0400

    test of the action see what happens

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    // get the matching keyword variable assigned
+    const keyword = core.getInput('keyword');
+    // get the GITHUB_TOKEN from input and use it to create an octokit client
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    console.log(`Keyword checked against this ${keyword}!`);
+    const { context } = require('@actions/github')
+    console.log("Printing out context");
+    console.log(context)
+
+} catch (error) {
+    core.setFailed(error.message);
+}
